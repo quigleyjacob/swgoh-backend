016ed82fc753f8230b1a502639bca4442d2f8893
Jacob Quigley <quigley.jacob3@icloud.com>
2023-01-15 07:18:28 -0500

    Add playerId to platoon data

--- platoons/Player.js
+++ platoons/Player.js
@@ -1,6 +1,7 @@
 export default class Player {
     constructor(playerData) {
         this.allyCode = playerData.allyCode
+        this.playerId = playerData.playerId
         this.roster = playerData.rosterMap
         this.name = playerData.name
         this.placements = this._initializePlacements()
