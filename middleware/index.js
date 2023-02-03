d12096eeeaf4b2a6242c8283525e5383197b71e0
Jacob Quigley <quigley.jacob3@icloud.com>
2023-02-03 15:57:48 -0500

    Fix middleware to whitelist routes that occur before session exists

--- middleware/index.js
+++ middleware/index.js
@@ -3,9 +3,13 @@ dotenv.config()
 import DB from '../lib/database.js'
 
 export async function validate(req, res, next) {
+    console.log(req.originalUrl)
     let discordKey = req.body.discordKey
     let session = req.body.session
-    if(discordKey && discordKey === process.env.DISCORD_API_KEY) {
+    let url = req.originalUrl
+    if(url === '/api/discord/authURL' || url === '/api/discord/authenticate') { //session is created from this route
+        next()
+    } else if(discordKey && discordKey === process.env.DISCORD_API_KEY) {
         next()
     } else if(session && await DB.verifySessionComplete(session)) {
         next()
