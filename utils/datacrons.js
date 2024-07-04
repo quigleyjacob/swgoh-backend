7e1733d8633d3eb2dc6df1d6539a2cfe1bc5f327
Jacob Quigley <quigley.jacob3@icloud.com>
2024-07-03 22:17:09 -0400

    Fix pow datacron filtering issue

--- utils/datacrons.js
+++ utils/datacrons.js
@@ -1,15 +1,25 @@
 
 // take POW affix from his template and add to base template, then remove POW template
 export function handlePOWTemplate(activeTemplates) {
-    let powTemplate = activeTemplates.find(template => template.id === 'datacron_set_15_padawanobiwan')
-    let powCharacterBonus = powTemplate.tier[8].affixTemplateSetId[0]
+    handleCharacterTemplate(activeTemplates, 'datacron_set_15_padawanobiwan', 'datacron_set_15_base')
+//     let powTemplate = activeTemplates.find(template => template.id === 'datacron_set_15_padawanobiwan')
+//     let powCharacterBonus = powTemplate.tier[8].affixTemplateSetId[0]
 
-    let baseTemplate = activeTemplates.find(template => template.id === 'datacron_set_15_base')
-    baseTemplate.tier[8].affixTemplateSetId.push(powCharacterBonus)
+//     let baseTemplate = activeTemplates.find(template => template.id === 'datacron_set_15_base')
+//     baseTemplate.tier[8].affixTemplateSetId.push(powCharacterBonus)
 
-    // delete POW template
-    let indexToRemove = activeTemplates.indexOf(template => template.id === 'datacron_set_15_padawanobiwan')
-    activeTemplates.splice(indexToRemove, 1)
+//     const getIndexOfCharacterTemplate = (template) => {
+//         console.log(template.id === 'datacron_set_15_padawanobiwan')
+//         return template.id === 'datacron_set_15_padawanobiwan'
+// }
+
+//     // delete POW template
+//     // let indexToRemove = activeTemplates.indexOf(getIndexOfCharacterTemplate)
+//     activeTemplates = activeTemplates.filter(template => template.id !== 'datacron_set_15_padawanobiwan')
+}
+
+export function handleNightTrooperTemplate(activeTemplates) {
+    handleCharacterTemplate(activeTemplates, 'datacron_set_16_nighttrooper', 'datacron_set_16_base')
 }
 
 export function getTags(affix) {
@@ -18,7 +28,19 @@ export function getTags(affix) {
         case 'datacron_character_padawanobiwan_002':
         case 'datacron_character_padawanobiwan_003':
             return ['lightside', 'galacticrepublic']
+        case 'datacron_character_nighttrooper_001':
+        case 'datacron_character_nighttrooper_002':
+        case 'datacron_character_nighttrooper_003':
+            return ['darkside', 'attacker']
         default:
             return affix.tag
     }
+}
+
+function handleCharacterTemplate(activeTemplates, characterTemplateName, templateBaseName) {
+    let template = activeTemplates.find(template => template.id === characterTemplateName)
+    let characterBonus = template.tier[8].affixTemplateSetId[0]
+
+    let baseTemplate = activeTemplates.find(template => template.id === templateBaseName)
+    baseTemplate.tier[8].affixTemplateSetId.push(characterBonus)
 }
