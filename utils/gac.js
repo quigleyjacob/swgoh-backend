16e99345278d96f78a1fad03f088d25fad53c9d3
Jacob Quigley <quigley.jacob3@icloud.com>
2025-02-08 00:34:53 -0500

    fix

--- utils/gac.js
+++ utils/gac.js
@@ -82,7 +82,8 @@ export function mergeGACPlans(oldGacPlan, newGacPlan) {
             } else {
                 oldGacPlan[owner][squadId].squad = newSquadData.squad.map((unit, index) => {
                     let oldUnit = oldGacPlan[owner][squadId].squad[index]
-                    return {...oldUnit, ...unit}
+                    console.log(oldUnit, unit)
+                    return {...unit, isAlive: oldUnit.isAlive}
                 })
                 oldGacPlan[owner][squadId].datacron = newSquadData.datacron
             }
